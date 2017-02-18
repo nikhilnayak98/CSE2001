dc021741777b3476dcee377a3725db5d1479c7b2
Nikhil Nayak <nikhilnayak98@gmail.com>
2017-02-18 11:52:55 +0530

    Add files via upload

--- Assignment3/Java/TowerOfHanoi.java
+++ Assignment3/Java/TowerOfHanoi.java
@@ -2,33 +2,33 @@
  * Author: Nikhil Ranjan Nayak
  * Regd.no: 1641012040
  * Branch & Sec: CSE 'F'
- * Brief Desc:  Towers of Hanoi puzzle
+ * Brief Desc:  Tower of Hanoi puzzle
  */
 
 import java.util.Scanner;
 
 public class TowerOfHanoi {
-	
+
 	static int moves = 1;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int source=1,temp=2,target=3;
+		int source=1,temp=3,target=2;
 		System.out.println("Enter no of discs - ");
-		int discs = sc.nextInt();
-		solve(discs,source,target,temp);
+		int n = sc.nextInt();
+		func(n,source,temp,target);
 		System.out.println("Total no of moves - " + moves);
 	}
-	
-	public static void solve(int n,int source,int temp,int target) {
-	       if (n == 1) {
-	           System.out.println(source + " to " + target);
-	       } else {
-	    	   moves++;
-	           solve(n-1,source,target,temp);
-	           System.out.println(source + " to " + target);
-	           moves++;
-	           solve(n-1,temp,source,target);
-	       }
-	   }
+
+	public static void func(int n,int source,int temp,int target) {
+		if(n==1) {
+			System.out.println("Move " + n + " from peg " + source + " to peg " + target);
+		} else {
+			moves++;
+			func(n-1,source,target,temp);
+			System.out.println("Move " + n + " from peg " + source + " to peg " + target);
+			moves++;
+			func(n-1,temp,source,target);
+		}
+	}
 }
